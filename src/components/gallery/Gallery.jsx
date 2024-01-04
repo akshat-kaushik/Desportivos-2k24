b6bce4214ad2a1a5a8c0e65cb08ca47f6a4562b0
akshat-kaushik <akshatkaushik9130@gmail.com>
2024-01-05 02:27:23 +0530

    testtt

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -6,27 +6,27 @@ function Gallery() {
     <div className="bg-black h-screen flex items-center justify-center">
       <div className="h-screen w-screen relative">
         <img
-          className="absolute top-1/3 rotate-180 -left-32"
+          className="absolute top-1/3 -left-32"
           src="galleryRock1.png"
           alt=""
         />
         <img
-          className="absolute bottom-16 right-0 rotate-180"
+          className="absolute bottom-16 right-0"
           src="galleryRock2.png"
           alt=""
         />
         <div className="mt-2">
           <Marquee direction="right" speed="100">
-            <img className="w-full" src="1.png" alt="" />
-            <img className="w-full" src="2.png" alt="" />
-            <img className="w-full" src="3.png" alt="" />
-            <img className="w-full" src="4.png" alt="" />
-            <img className="w-full" src="5.png" alt="" />
-            <img className="w-full" src="6.png" alt="" />
-            <img className="w-full" src="7.png" alt="" />
-            <img className="w-full" src="8.png" alt="" />
-            <img className="w-full" src="9.png" alt="" />
-            <img className="w-full " src="10.png" alt="" />
+            <img className="w-full mx-1" src="1.png" alt="" />
+            <img className="w-full mx-1" src="2.png" alt="" />
+            <img className="w-full mx-1" src="3.png" alt="" />
+            <img className="w-full mx-1" src="4.png" alt="" />
+            <img className="w-full mx-1" src="5.png" alt="" />
+            <img className="w-full mx-1" src="6.png" alt="" />
+            <img className="w-full mx-1" src="7.png" alt="" />
+            <img className="w-full mx-1" src="8.png" alt="" />
+            <img className="w-full mx-1" src="9.png" alt="" />
+            <img className="w-full mx-1" src="10.png" alt="" />
           </Marquee>
         </div>
       </div>
@@ -35,3 +35,4 @@ function Gallery() {
 }
 
 export default Gallery;
+

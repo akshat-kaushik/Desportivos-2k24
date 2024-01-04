49d6d949b072cefb868f6a0cd14f9dcff03d06f0
sourpie <111598221+sourpie@users.noreply.github.com>
2024-01-05 03:01:48 +0530

    push4

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -17,22 +17,22 @@ const Home = () => {
   return (
     <div className='bg-black relative w-full min-h-screen '>
 
-      <div id='bg' className="p-0 m-0 bg-homemob sm:bg-homepc min-h-screen bg-no-repeat bg-cover absolute top-0 left-0 w-full  bg-fixed " >
+      <div id='bg' className="p-0 m-0 bg-homemob sm:bg-homepc min-h-screen bg-no-repeat bg-cover absolute top-0 left-0 w-full  md:bg-fixed " >
         <Navbar />
 
-        <div className="fixed bottom-28 md:bottom-2 flex justify-center align-middle mb-2 w-full z-10">
+        <div className="md:fixed absolute bottom-28 md:bottom-2 flex justify-center align-middle mb-2 w-full z-10">
           <img className="object-center h-26 sm:h-32 w-[75%]" src="src/components/images/desportivous.png" alt="despo" />
         </div>
 
         <img className='hidden md:block fixed -translate-x-1/2  left-1/2 bottom-10 md:-bottom-60 ' src='src/components/images/heros.png' />
         {/* <img className='hidden md:block fixed -translate-x-1/2 w-[52rem] left-[52.8rem]  md:bottom-[2rem] ' src='src/components/images/power.gif' /> */}
-        <img className=' md:hidden fixed -translate-x-1/2  left-1/2 bottom-40 scale-[2] ' src='src/components/images/heros.png' />
+        <img className=' md:hidden absolute -translate-x-1/2  left-1/2 bottom-44 scale-[2] ' src='src/components/images/heros.png' />
 
 
-        <img className='fixed -translate-x-1/2 -translate-y-1/2 left-1/2 bottom-44 md:bottom-24' src='src/components/images/boards.png' />
+        <img className='md:fixed absolute -translate-x-1/2 -translate-y-1/2 left-1/2 bottom-44 md:bottom-24' src='src/components/images/boards.png' />
 
         <a href=''>
-          <img className='md:hidden fixed -translate-x-1/2 -translate-y-1/2 left-1/2 bottom-6' src='src/components/images/Register01.png' />
+          <img className='md:hidden absolute -translate-x-1/2 -translate-y-1/2 left-1/2 bottom-6' src='src/components/images/Register01.png' />
         </a>
 
 

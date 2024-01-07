65643b72180fe5756229ae6267478e2ce3319930
akshat-kaushik <akshatkaushik9130@gmail.com>
2024-01-08 00:32:15 +0530

    alldone

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         scrollTrigger: {
           trigger: comp.current,
           start: "top",
-          end: "bottom",
+          end: "middle",
           once: true,
         },
       });
